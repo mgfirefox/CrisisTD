fc022ee006be728e591169a510224c04cdb274e9
mgfirefox <ma2ks2im8ka228@gmail.com>
2025-11-15 03:23:57 +0300

    feat: add constructors and default value to struct

--- Assets/Project/Script/Struct/Result/TranslationTo/TranslationToResult.cs
+++ Assets/Project/Script/Struct/Result/TranslationTo/TranslationToResult.cs
@@ -4,7 +4,22 @@ namespace Mgfirefox.CrisisTd
 {
     public struct TranslationToResult
     {
+        public static TranslationToResult Zero { get; } = new()
+        {
+            position = Vector3.zero,
+        };
+        
         public Vector3 position;
         public float travelledDistance;
+
+        public TranslationToResult(Vector3 position) : this()
+        {
+            this.position = position;
+        }
+
+        public TranslationToResult(Vector3 position, float travelledDistance) : this(position)
+        {
+            this.travelledDistance = travelledDistance;
+        }
     }
 }
