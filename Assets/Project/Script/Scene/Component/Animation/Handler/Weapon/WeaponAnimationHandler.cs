7f74f128748fdb24bad566d42e2f569984757bbc
mgfirefox <ma2ks2im8ka228@gmail.com>
2025-11-23 00:21:56 +0300

    feat: reimport rigs, models and animations; add textures and audio to single target attack test tower

---
+++ Assets/Project/Script/Scene/Component/Animation/Handler/Weapon/WeaponAnimationHandler.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Mgfirefox.CrisisTd
+{
+    public class WeaponAnimationHandler : MonoBehaviour
+    {
+        [SerializeField]
+        [BoxGroup("Dependencies")]
+        [Required]
+        private AudioSource attackAudio;
+
+        private void OnAnimationAttack()
+        {
+            attackAudio.Play();
+        }
+    }
+}
